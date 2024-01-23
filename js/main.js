2019202e3bd224d6de143373824056192fab157e
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-01-23 15:59:34 +0100

    feat add milestone 1 request

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// array di persone (type object)
+const team = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    image: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    image: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    image: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    image: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    image: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+// stampa in console della persona
+for (let persona of team) {
+  for (let attribute in persona) {
+    console.log(`${attribute}: ` + persona[attribute]);
+  }
+}
